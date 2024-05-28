8c29bab904eb38a9e9d3f8eb79c49df92cd9f002
Hasibul <skhasibulhossain16@gmail.com>
2024-05-28 13:14:57 +0530

    api update issue fixed

--- src/components/pages/login/Signin.js
+++ src/components/pages/login/Signin.js
@@ -30,9 +30,10 @@ const Signin = () => {
       );
       const data = response.data;
       const token = data.token;
+      console.log(data.data.user.name);
       if (token) {
         sessionStorage.setItem("userToken", token);
-        sessionStorage.setItem("userName", JSON.stringify(data.data.name));
+        sessionStorage.setItem("userName", JSON.stringify(data.data.user.name));
         setIsLogin(true);
         // ispasswordHide(false);
         if (state) {
